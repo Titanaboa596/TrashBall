bb86e10d0aed011f3bb172bfbc5dff4fbce3ec60
Titanaboa596 <116961326+Titanaboa596@users.noreply.github.com>
2023-02-11 08:47:07 -0400

    Add files via upload

---
+++ MatterJSBoilerPlate-main/MatterJSBoilerPlate-main/sketch.js
@@ -0,0 +1,50 @@
+var ball
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+
+	engine = Engine.create();
+	world = engine.world;
+
+	balls_options = {
+		isStatic: false,
+		restitution:0.3,
+		friction:0,
+		density:1.2,
+
+	}
+
+	//Matter.Bodies.circle(x, y, radius, [options], [maxSides])
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  
+  groundObj.display();
+  leftSide.display();
+  rightSide.display();
+
+  drawSprites();
+ 
+}
+
+
+
